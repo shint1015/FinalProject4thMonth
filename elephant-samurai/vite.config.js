f498c7ebeb14e8a6355b10177dd58517459fac66
Shintaro Miyata <shintaroot10@gmail.com>
2025-11-04 17:12:17 -0800

    setting config alias path

--- elephant-samurai/vite.config.js
+++ elephant-samurai/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     build: {
         target: 'es6',
         modulePreload: { polyfill: true },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 })
