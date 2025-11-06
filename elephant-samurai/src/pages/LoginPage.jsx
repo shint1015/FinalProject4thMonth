85c7efa25c937aee7a33479c4808b7eac5be4b40
Shintaro Miyata <shintaroot10@gmail.com>
2025-11-05 18:57:57 -0800

    update

--- elephant-samurai/src/pages/LoginPage.jsx
+++ elephant-samurai/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import login_background from '@/assets/img/login_background.svg'
+import LoginForm from '@/components/auth/LoginForm.jsx'
+
 export default function LoginPage() {
     return (
         <div class='flex'>
             <img src={login_background} alt='Login Background' />
-            <form></form>
+            <LoginForm />
         </div>
     )
 }
