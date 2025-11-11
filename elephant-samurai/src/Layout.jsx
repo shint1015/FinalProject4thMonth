f97e8d3d2ef5470e90f97b6311c9bf183897475f
daiki-ebisuya <ebisuya0510daiki@gmail.com>
2025-11-11 03:35:38 -0800

    manage footer while detail

--- elephant-samurai/src/Layout.jsx
+++ elephant-samurai/src/Layout.jsx
@@ -1,13 +1,23 @@
 import Footer from '@/components/layout/Footer'
 import Header from '@/components/layout/Header'
+import { useRouterState } from '@tanstack/react-router';
+
 
 const Layout = ({ children }) => {
+
+    // Get the current path using useRouterState for conditional footer rendering
+    const {location} = useRouterState();
+    const path = location.pathname;
+    const hiddenFooter = path.startsWith('/show/');
+    console.log('Current Page:', path);
+
     return (
         <div className='bg-primary-black text-primary-white'>
             <Header />
             <main>{children}</main>
-            <Footer />
+            {!hiddenFooter && <Footer />}
         </div>
+        
     )
 }
 
