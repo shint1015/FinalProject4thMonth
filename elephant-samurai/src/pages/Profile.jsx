f761cc54291c671ee8f5de6dc1e6cd00386e4ba7
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-11-13 00:28:18 -0800

    fix sub nav bar + completed structure profile

--- elephant-samurai/src/pages/Profile.jsx
+++ elephant-samurai/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from '@tanstack/react-router'
 
 // call default from API to add info in place hodler
 
-// call default from API
+// call default from API to add info in place hodler
 
 export default function Profile(){
     const { user, isLoading, isAuthenticated } = useAuth()
@@ -25,6 +25,38 @@ export default function Profile(){
         <div className='px-[2rem] sm:px-[3rem] lg:px-[5rem]'>
             <h4 className="mt-8 text-primary-yellow text-h4 mb-6">Welcome back {user.name}!</h4>
             <SubMenu/>
+            {/* form */}
+            <form className='mt-4'>
+                <div>
+                    <p className='text-subbody text-dark-gray'>First Name</p>
+                    <input type="text" 
+                    placeholder="" 
+                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
+                    />
+                </div>
+                <div>
+                    <p className='text-subbody text-dark-gray'>Last Name</p>
+                    <input type="text" 
+                    placeholder="" 
+                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
+                    />
+                </div>    
+                <div>
+                    <p className='text-subbody text-dark-gray'>Email</p>
+                    <input type="text" 
+                    placeholder="" 
+                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
+                    />
+                </div> 
+                <div>
+                    <p className='text-subbody text-dark-gray'>Password</p>
+                    <input type="text" 
+                    placeholder="" 
+                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
+                    />
+                </div>   
+                <button type="submit" className="bg-primary-yellow text-black py-3 px-6 my-6 rounded hover:bg-secondary-yellow text-subbody">Update Profile</button>
+            </form>                                 
         </div>
         <Outlet /> 
         </>
