828960bb974a4a5f06149e2e8e0153a76a7d3d9d
Shintaro Miyata <shintaroot10@gmail.com>
2025-11-12 23:47:42 -0800

    bug fix

--- elephant-samurai/src/route.jsx
+++ elephant-samurai/src/route.jsx
@@ -103,7 +103,6 @@ export const myTicketsRoute = createRoute({
   component: () => <MyTickets />,
 })
 
-
 const loginRoute = createRoute({
     getParentRoute: () => rootRoute,
     path: '/login',
