e0d82672b1d26041fa38ce2a83d52b981b7d44a7
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-11-14 17:04:26 -0800

    Fix Route
    Right now The Profile is just layout header and move info profile to profile form page

--- elephant-samurai/src/pages/Profile.jsx
+++ elephant-samurai/src/pages/Profile.jsx
@@ -25,38 +25,6 @@ export default function Profile(){
         <div className='px-[2rem] sm:px-[3rem] lg:px-[5rem]'>
             <h4 className="mt-8 text-primary-yellow text-h4 mb-6">Welcome back {user.name}!</h4>
             <SubMenu/>
-            {/* form */}
-            <form className='mt-4'>
-                <div>
-                    <p className='text-subbody text-dark-gray'>First Name</p>
-                    <input type="text" 
-                    placeholder="" 
-                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
-                    />
-                </div>
-                <div>
-                    <p className='text-subbody text-dark-gray'>Last Name</p>
-                    <input type="text" 
-                    placeholder="" 
-                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
-                    />
-                </div>    
-                <div>
-                    <p className='text-subbody text-dark-gray'>Email</p>
-                    <input type="text" 
-                    placeholder="" 
-                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
-                    />
-                </div> 
-                <div>
-                    <p className='text-subbody text-dark-gray'>Password</p>
-                    <input type="text" 
-                    placeholder="" 
-                    className="bg-primary-white border-none placeholder-dark-grey text-black text-subbody focus:outline-none p-[1rem] rounded-[4px]"
-                    />
-                </div>   
-                <button type="submit" className="bg-primary-yellow text-black py-3 px-6 my-6 rounded hover:bg-secondary-yellow text-subbody">Update Profile</button>
-            </form>                                 
         </div>
         <Outlet /> 
         </>
