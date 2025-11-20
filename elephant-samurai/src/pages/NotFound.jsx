455fd008ea34b33550500c8b552994ec78a6c05d
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-11-20 14:06:56 -0800

    Fix photo error

--- elephant-samurai/src/pages/NotFound.jsx
+++ elephant-samurai/src/pages/NotFound.jsx
@@ -1,10 +1,10 @@
-import Group from "@/assets/img/Group.png";
+import errorPhoto from "@/assets/img/error.svg";
 import { Link } from "@tanstack/react-router";
 
 export default function notFound() {
     return (
         <div className="bg-primary-black my-[15%] text-primary-white flex flex-col items-center px-4 track-[0.03rem] relative">
-            <img src={Group} alt="404 Not Found" className="w-1/2 max-w-[35%] absolute mt-[-8%] mb-4" />
+            <img src={errorPhoto} alt="404 Not Found" className="w-1/2 max-w-[35%] absolute mt-[-8%] mb-4" />
             <h1 className="text-display text-primary-yellow relative mt-[4%]">404</h1>
             <p className="mt-[-0.5%] text-subtitle text-primary-yellow mb-[3%] ">Page Not Found</p>
             
